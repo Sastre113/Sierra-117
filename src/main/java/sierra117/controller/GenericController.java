a7cdd8b7f96303ccaf1b47b80f5396bae0b7530b
Miguel Á. Sastre <sastre113@gmail.com>
2023-12-12 22:26:31 +0100

    Configuración de base de datos h2

--- src/main/java/sierra117/controller/GenericController.java
+++ src/main/java/sierra117/controller/GenericController.java
@@ -3,6 +3,9 @@
  */
 package sierra117.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GenericController {
 
+	@GetMapping(path = "/generic/{nameTable}")
+	public <T> ResponseEntity<T> getTable(@PathVariable String nameTable){
+		return null;
+	}
+	
 }
