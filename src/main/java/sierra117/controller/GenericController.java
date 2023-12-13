04811b1e17c3bb8a0f657a5fb2e342deafa4cdb7
Miguel Á. Sastre <sastre113@gmail.com>
2023-12-13 23:17:45 +0100

    test

--- src/main/java/sierra117/controller/GenericController.java
+++ src/main/java/sierra117/controller/GenericController.java
@@ -4,8 +4,10 @@
 package sierra117.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class GenericController {
 
 	@GetMapping(path = "/generic/{nameTable}")
-	public <T> ResponseEntity<T> getTable(@PathVariable String nameTable){
-		return null;
+	public <T> ResponseEntity<String> getTable(@PathVariable String nameTable){
+		return ResponseEntity.ok("Tabla " + nameTable + " obtenida!");
+	}
+	
+	@PostMapping(path = "/generic/{nameTable}")
+	public <T> ResponseEntity<String> postTable(@PathVariable String nameTable){
+		return ResponseEntity.ok("Post " + nameTable + " method http!");
+	}
+	
+	@DeleteMapping(path = "/generic/{nameTable}") 
+	public ResponseEntity<String> deleteTable(@PathVariable String nameTable){
+		return ResponseEntity.ok("Borrado "+ nameTable + " con exito!");
 	}
 }
